0c2bbfe89737dd7e97032dd84c7cb2e215891b85
vinayjoshi434 <vinayjoshi434@gmail.com>
2025-07-29 16:24:09 +0530

    changes done in user model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -17,7 +17,7 @@ const Userschema = new mongoose.Schema(
         username: {
             type: String,
             required: true,
-
+            unique: true,
             lowercase: true
 
         },
@@ -25,6 +25,7 @@ const Userschema = new mongoose.Schema(
             type: String,
             required: true,
             lowercase: true,
+            unique: true
 
         },
         password: {
